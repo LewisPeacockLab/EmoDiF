7883b5ebdc86730db8f67e04e28a2bf753343fbe
twang04 <tw24955@dhcp-128-83-97-125.psy.utexas.edu>
2019-06-15 20:22:47 -0500

    fixed my mistake in reversing commits

---
+++ Analysis/MVPA/emodif_word_scene_tradeoff_ana.m
@@ -0,0 +1,12 @@
+function emodif_word_scene_separation()
+
+%to where results directory is
+load('emodif_aggregate_results_complete.mat')
+
+% across R
+% across F 
+% across both
+
+%collapse across ALL the trials - 
+R_data = [];
+F_data = [];
