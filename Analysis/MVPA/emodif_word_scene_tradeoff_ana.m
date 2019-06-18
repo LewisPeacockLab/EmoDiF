8de02c2930a20a2f7c35d5dde9a646d6c31a03c8
twang04 <twang04@Piglet.local>
2019-06-18 14:13:08 -0500

    added file save

--- Analysis/MVPA/emodif_word_scene_tradeoff_ana.m
+++ Analysis/MVPA/emodif_word_scene_tradeoff_ana.m
@@ -1,17 +1,15 @@
-function emodif_word_scene_separation()
+function emodif_word_scene_tradeoff_ana()
 
 %to where results directory is
 load('emodif_aggregate_results_complete.mat')
 
-% across R
-% across F 
-% across both
-
-%collapse across ALL the trials - 
+%load data
 R_data_word = [];
 F_data_word = [];
 R_data_scene = [];
 F_data_scene = [];
+
+%get all subjects all trials
 
 for s = 1:24
     r_data_w = results.bysubject.data(s).results.remember.all.acts.word;
@@ -29,10 +27,17 @@ for s = 1:24
     F_corr=[];
     R_corrP=[];
     F_corrP=[];
+    
+    %correlate trials word and scene
 
     for x = 1:length(R_data_scene)
         [R_correl(x,1), R_corrP(x,1)] = corr(R_data_word(x,:)',R_data_scene(x,:)');
         [F_correl(x,1), F_corrP(x,1)] = corr(F_data_word(x,:)',F_data_scene(x,:)');
+%Fisher's transform 
+        
+%FISHER Equation
+%z = 0.5*log((1+r)/(1-r));
+
         R_correl(x,2) = 0.5*log((1+R_correl(x,1))/(1-R_correl(x,1)));
         F_correl(x,2) = 0.5*log((1+F_correl(x,1))/(1-F_correl(x,1)));
         
@@ -46,17 +51,26 @@ for s = 1:24
         FPperc = FPcount/length(F_data_scene);
         
     end
-    results_tradeoff.bysubject.
     
-    %Fisher's transform 
-        
-%FISHER Equation
-%z = 0.5*log((1+r)/(1-r));
+    results_tradeoff.Rcorrel = R_correl;
+    results_tradeoff.Fcorrel = F_correl;
+    results_tradeoff.R_correl_mean = R_correl_mean_z;
+    results_tradeoff.F_correl_mean = F_correl_mean_z;
+    results_tradeoff.RPcount = RPcount;
+    results_tradeoff.FPcount = FPcount;
+    results_tradeoff.RPperc = RPperc;
+    results_tradeoff.FPperc = FPperc;
+    results_tradeoff.R_data_w = R_data_word;
+    results_tradeoff.F_data_s = F_data_scene;
+    results_tradeoff.R_data_s = R_data_scene;
+    results_tradeoff.F_data_w = F_data_word;
+outfilename = 'emodif_tradeoff_ana.mat';
+
+save(outfilename,'results_tradeoff');
     
     
 end
 
 
 
-    results_tradeoff.R_data_w = R_data_words;
-    results_tradeoff.F_data_s = F_data_scenes;
+
